c4876e7f8903256b07d23ff84d3976988d23fe23
MKMonkeyCat <a102009102009@gmail.com>
2026-03-28 15:40:43 +0000

    fix: Move import statement for main inside CLI condition to improve module loading

--- wak2/main.py
+++ wak2/main.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 import sys
 
-from project.__main__ import main
-
 if __name__ == "__main__":
     if sys.argv[1].endswith("--cli"):
+        from project.__main__ import main
+
         main()
     else:
         from project.web import run_web
