b17236a2597efaca51af4f84ddae11fcf8a05766
MKMonkeyCat <a102009102009@gmail.com>
2026-03-28 15:35:21 +0000

    feat: Implement web interface for voting system, enhancing user interaction and navigation

--- wak2/main.py
+++ wak2/main.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+import sys
+
 from project.__main__ import main
 
 if __name__ == "__main__":
-    main()
+    if sys.argv[1].endswith("--cli"):
+        main()
+    else:
+        from project.web import run_web
+
+        run_web()
