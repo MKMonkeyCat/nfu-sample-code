73f2d3730a48a674b09dbca442421934382ab94a
MKMonkeyCat <a102009102009@gmail.com>
2026-03-29 03:57:27 +0000

    fix: Add length check for command-line arguments before accessing to prevent index error

--- wak2/main.py
+++ wak2/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import sys
 
 if __name__ == "__main__":
-    if sys.argv[1].endswith("--cli"):
+    if len(sys.argv) > 1 and sys.argv[1].endswith("--cli"):
         from project.__main__ import main
 
         main()
